2c6ba315de04e17d8908722c1f09feb9d1d9c139
Jeremiah Matthews <jeremiah.matthews105@gmail.com>
2023-11-13 07:43:31 -0600

    Local Storage

--- Whiteboard Questions/questions.js
+++ Whiteboard Questions/questions.js
@@ -3,13 +3,14 @@
 //TODO:Reverse a String example hello
 function reverseString(str) {
     let strArray = str.split('');
-    let reversedArray = strArray.reverse();
-    let reversedString = reversedArray.join('');
-    return reversedString;
+    // let reversedArray = strArray.reverse();
+    // let reversedString = reversedArray.join('');
+    // return reversedString;
+    return strArray;
 }
 console.log(reverseString("hello"));
 
-//TODO: Write a program that prints the numbers from 1-100. For multiples of three print Fizz, for multiples of 5 print Buzz for multiples pf 3 and 5 print FizzBuzz.
+//TODO: Write a program that prints the numbers from 1-100. For multiples of three print Fizz, for multiples of 5 print Buzz for multiples of 3 and 5 print FizzBuzz.
 
 // for (let i = 1; i <= 50; i++) {
 //     if (i % 3 === 0 && i % 5 === 0) {
@@ -19,26 +20,26 @@ console.log(reverseString("hello"));
 //     } else if (i % 5 === 0) {
 //         console.log("Buzz");
 //     } else {
-//         // console.log(i);
+//         console.log(i);
 //     }
 // }
 
 // //TODO: Write the FizzBuzz as a switch statement
-// for (let j = 1; j <= 60; j++) {
-//     switch (true) {
-//         case j % 3 === 0 && j % 5 === 0:
-//             console.log("FizzBuzz");
-//             break;
-//         case j % 3 === 0:
-//             console.log("Fizz");
-//             break;
-//         case j % 5 === 0:
-//             console.log("Buzz");
-//             break;
-//         default:
-//             // console.log(j);
-//     }
-// }
+for (let j = 1; j <= 60; j++) {
+    switch (true) {
+        case j % 3 === 0 && j % 5 === 0:
+            console.log("FizzBuzz");
+            break;
+        case j % 3 === 0:
+            console.log("Fizz");
+            break;
+        case j % 5 === 0:
+            console.log("Buzz");
+            break;
+        default:
+            console.log(j);
+    }
+}
 
 //TODO: Palindrome Check: Write a function to determine if a given string is a palindrome -- Example radar
 
