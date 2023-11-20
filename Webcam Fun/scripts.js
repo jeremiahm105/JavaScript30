aa86246cca915eb931168db3866a2a76d9ff9029
Jeremiah Matthews <jeremiah.matthews105@gmail.com>
2023-11-20 07:32:40 -0600

    updated js

--- Webcam Fun/scripts.js
+++ Webcam Fun/scripts.js
@@ -3,3 +3,51 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+
+
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch(err => {
+            console.error(`OH NO!!!`, err);
+        });
+}
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        // take the pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+
+        // pixels = redEffect(pixels);
+
+        pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.8;
+
+        // pixels = greenScreen(pixels);
+
+        ctx.putImageData(pixels, 0, 0);
+    }, 16);
+}
+function takePhoto() {
+    // played the sound
+    snap.currentTime = 0;
+    snap.play();
+
+    // take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+    strip.insertBefore(link, strip.firstChild);
+}
