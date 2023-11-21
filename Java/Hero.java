6c5e9f5e9979b7b9dba1147c0227de0e17fd70a7
Jeremiah Matthews <jeremiah.matthews105@gmail.com>
2023-11-21 15:22:56 -0600

    java oop

---
+++ Java/Hero.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class Hero extends Person{
+    String power;
+    int age;
+    Hero(String name, int age, String power) {
+        super(name,age);
+        this.power = power;
+
+    }
+
+}
