1ff1706955b41468643d8fd3f04e861773fb75ed
Jeremiah Matthews <jeremiah.matthews105@gmail.com>
2023-11-24 17:27:38 -0600

    updated files

--- Webcam Fun/scripts.js
+++ Webcam Fun/scripts.js
@@ -76,8 +76,10 @@ function greenScreen(pixels) {
         levels[input.name] = input.value;
     });
 
+
+
     //Remove after refactor
-    function rgbSplit(pixels) {
+    function rgbSplits(pixels) {
         for (let i = 0; i < pixels.data.length; i+=4) {
             pixels.data[i - 150] = pixels.data[i + 0]; // RED
             pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
@@ -85,7 +87,7 @@ function greenScreen(pixels) {
         }
         return pixels;
     }
-    function rgbSplitsFor(pixels) {
+    function rgbSplitsForPixesl(pixels) {
         for (let i = 0; i < pixels.data.length; i+=4) {
             pixels.data[i - 150] = pixels.data[i + 0]; // RED
             pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
@@ -93,5 +95,49 @@ function greenScreen(pixels) {
         }
         return pixels;
     };
+
+    //refactor
+    function paintToCanvases() {
+        const width = video.videoWidth;
+        const height = video.videoHeight;
+        canvas.width = width;
+        canvas.height = height;
+
+        return setInterval(() => {
+            ctx.drawImage(video, 0, 0, width, height);
+            // take the pixels out
+            let pixels = ctx.getImageData(0, 0, width, height);
+
+            // pixels = redEffect(pixels);
+
+            pixels = rgbSplit(pixels);
+            // ctx.globalAlpha = 0.8;
+
+            // pixels = greenScreen(pixels);
+
+            ctx.putImageData(pixels, 0, 0);
+        }, 16);
+    }
+    function takePhotos() {
+        // played the sound
+        snap.currentTime = 0;
+        snap.play();
+
+        // take the data out of the canvas
+        const data = canvas.toDataURL('image/jpeg');
+        const link = document.createElement('a');
+        link.href = data;
+        link.setAttribute('download', 'handsome');
+        link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+        strip.insertBefore(link, strip.firstChild);
+    }
+    function redEffect(pixels) {
+        for (let i = 0; i < pixels.data.length; i+=4) {
+            pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+            pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+            pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+        }
+        return pixels;
+    }
 
     //refactor
